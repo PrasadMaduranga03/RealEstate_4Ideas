ccad2e2e6ae3eccfc6c977dad246e9e50408d1f1
taniyadilshani <taniyapalihakkara@gmail.com>
2015-03-28 15:49:29 +0530

    design the interface

--- src/com/household/main/RealEstate.java
+++ src/com/household/main/RealEstate.java
@@ -1,10 +1,17 @@
 package com.household.main;
 
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.JLabel;
+
+
 
 public class RealEstate {
+	
+	private JFrame frmRealEstate;
+	
 
 	private JFrame frame;
 
@@ -32,12 +39,48 @@ public class RealEstate {
 	}
 
 	/**
-	 * Initialise the contents of the frame.
+	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 450, 349);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel_4 = new JLabel("New label");
+		lblNewLabel_4.setBounds(10, 11, 46, 14);
+		frame.getContentPane().add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_5 = new JLabel("New label");
+		lblNewLabel_5.setBounds(102, 65, 46, 14);
+		frame.getContentPane().add(lblNewLabel_5);
+		
+		JLabel lblNewLabel_6 = new JLabel("New label");
+		lblNewLabel_6.setBounds(102, 90, 46, 14);
+		frame.getContentPane().add(lblNewLabel_6);
+		
+		JLabel lblNewLabel_7 = new JLabel("New label");
+		lblNewLabel_7.setBounds(102, 115, 46, 14);
+		frame.getContentPane().add(lblNewLabel_7);
+		
+		JLabel lblNewLabel_8 = new JLabel("New label");
+		lblNewLabel_8.setBounds(102, 140, 46, 14);
+		frame.getContentPane().add(lblNewLabel_8);
+		
+		JLabel lblNewLabel_9 = new JLabel("New label");
+		lblNewLabel_9.setBounds(102, 167, 46, 14);
+		frame.getContentPane().add(lblNewLabel_9);
+		
+		JLabel lblNewLabel_10 = new JLabel("New label");
+		lblNewLabel_10.setBounds(102, 190, 46, 14);
+		frame.getContentPane().add(lblNewLabel_10);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(10, 215, 200, 50);
+		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		btnNewButton_1.setBounds(220, 215, 200, 50);
+		frame.getContentPane().add(btnNewButton_1);
 	}
-
 }
