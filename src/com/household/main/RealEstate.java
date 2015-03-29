0b539879392eebf2a0d754f503552a461cbc0aba
taniyadilshani <taniyapalihakkara@gmail.com>
2015-03-29 15:06:08 +0530

    desing the buttons in the interface

--- src/com/household/main/RealEstate.java
+++ src/com/household/main/RealEstate.java
@@ -5,12 +5,14 @@ import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
 import javax.swing.JLabel;
+import java.awt.Color;
+import java.awt.Font;
 
 
 
 public class RealEstate {
 	
-	private JFrame frmRealEstate;
+	
 	
 
 	private JFrame frame;
@@ -21,7 +23,7 @@ public class RealEstate {
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
-				try {
+			 	try {
 					RealEstate window = new RealEstate();
 					window.frame.setVisible(true);
 				} catch (Exception e) {
@@ -43,36 +45,38 @@ public class RealEstate {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 349);
+		frame.setBounds(100, 100, 485, 468);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JLabel lblNewLabel_4 = new JLabel("New label");
-		lblNewLabel_4.setBounds(10, 11, 46, 14);
+		JLabel lblNewLabel_4 = new JLabel("Next house displayed");
+		lblNewLabel_4.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblNewLabel_4.setForeground(new Color(0, 0, 205));
+		lblNewLabel_4.setBounds(10, 11, 200, 20);
 		frame.getContentPane().add(lblNewLabel_4);
 		
-		JLabel lblNewLabel_5 = new JLabel("New label");
-		lblNewLabel_5.setBounds(102, 65, 46, 14);
+		JLabel lblNewLabel_5 = new JLabel("Lot Number");
+		lblNewLabel_5.setBounds(102, 65, 129, 14);
 		frame.getContentPane().add(lblNewLabel_5);
 		
-		JLabel lblNewLabel_6 = new JLabel("New label");
-		lblNewLabel_6.setBounds(102, 90, 46, 14);
+		JLabel lblNewLabel_6 = new JLabel("First Name");
+		lblNewLabel_6.setBounds(102, 90, 108, 14);
 		frame.getContentPane().add(lblNewLabel_6);
 		
-		JLabel lblNewLabel_7 = new JLabel("New label");
-		lblNewLabel_7.setBounds(102, 115, 46, 14);
+		JLabel lblNewLabel_7 = new JLabel("Last Name");
+		lblNewLabel_7.setBounds(102, 115, 108, 14);
 		frame.getContentPane().add(lblNewLabel_7);
 		
-		JLabel lblNewLabel_8 = new JLabel("New label");
+		JLabel lblNewLabel_8 = new JLabel("Price");
 		lblNewLabel_8.setBounds(102, 140, 46, 14);
 		frame.getContentPane().add(lblNewLabel_8);
 		
-		JLabel lblNewLabel_9 = new JLabel("New label");
-		lblNewLabel_9.setBounds(102, 167, 46, 14);
+		JLabel lblNewLabel_9 = new JLabel("Square Feet");
+		lblNewLabel_9.setBounds(102, 165, 108, 20);
 		frame.getContentPane().add(lblNewLabel_9);
 		
-		JLabel lblNewLabel_10 = new JLabel("New label");
-		lblNewLabel_10.setBounds(102, 190, 46, 14);
+		JLabel lblNewLabel_10 = new JLabel("Number Of Bedrooms");
+		lblNewLabel_10.setBounds(102, 190, 139, 14);
 		frame.getContentPane().add(lblNewLabel_10);
 		
 		JButton btnNewButton = new JButton("New button");
@@ -82,5 +86,21 @@ public class RealEstate {
 		JButton btnNewButton_1 = new JButton("New button");
 		btnNewButton_1.setBounds(220, 215, 200, 50);
 		frame.getContentPane().add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("New button");
+		btnNewButton_2.setBounds(10, 278, 200, 50);
+		frame.getContentPane().add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("New button");
+		btnNewButton_3.setBounds(220, 278, 200, 50);
+		frame.getContentPane().add(btnNewButton_3);
+		
+		JButton btnNewButton_4 = new JButton("New button");
+		btnNewButton_4.setBounds(10, 341, 200, 50);
+		frame.getContentPane().add(btnNewButton_4);
+		
+		JButton btnNewButton_5 = new JButton("New button");
+		btnNewButton_5.setBounds(220, 341, 200, 50);
+		frame.getContentPane().add(btnNewButton_5);
 	}
 }
