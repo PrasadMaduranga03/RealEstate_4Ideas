568363d0d0e2e382468efd6b8a74f922f9ac1fd8
taniyadilshani <taniyapalihakkara@gmail.com>
2015-03-29 15:40:45 +0530

    design the tet boxes and lables in  the interface

--- src/com/household/main/RealEstate.java
+++ src/com/household/main/RealEstate.java
@@ -7,6 +7,7 @@ import javax.swing.JOptionPane;
 import javax.swing.JLabel;
 import java.awt.Color;
 import java.awt.Font;
+import javax.swing.JTextField;
 
 
 
@@ -16,6 +17,12 @@ public class RealEstate {
 	
 
 	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
 
 	/**
 	 * Launch the application.
@@ -45,62 +52,98 @@ public class RealEstate {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 485, 468);
+		frame.setBounds(100, 100, 490, 593);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		JLabel lblNewLabel_4 = new JLabel("Next house displayed");
-		lblNewLabel_4.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblNewLabel_4.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		lblNewLabel_4.setForeground(new Color(0, 0, 205));
-		lblNewLabel_4.setBounds(10, 11, 200, 20);
+		lblNewLabel_4.setBounds(10, 11, 287, 31);
 		frame.getContentPane().add(lblNewLabel_4);
 		
 		JLabel lblNewLabel_5 = new JLabel("Lot Number");
-		lblNewLabel_5.setBounds(102, 65, 129, 14);
+		lblNewLabel_5.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		lblNewLabel_5.setBounds(45, 82, 129, 14);
 		frame.getContentPane().add(lblNewLabel_5);
 		
 		JLabel lblNewLabel_6 = new JLabel("First Name");
-		lblNewLabel_6.setBounds(102, 90, 108, 14);
+		lblNewLabel_6.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		lblNewLabel_6.setBounds(45, 118, 108, 14);
 		frame.getContentPane().add(lblNewLabel_6);
 		
 		JLabel lblNewLabel_7 = new JLabel("Last Name");
-		lblNewLabel_7.setBounds(102, 115, 108, 14);
+		lblNewLabel_7.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		lblNewLabel_7.setBounds(45, 154, 108, 14);
 		frame.getContentPane().add(lblNewLabel_7);
 		
 		JLabel lblNewLabel_8 = new JLabel("Price");
-		lblNewLabel_8.setBounds(102, 140, 46, 14);
+		lblNewLabel_8.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		lblNewLabel_8.setBounds(45, 182, 46, 14);
 		frame.getContentPane().add(lblNewLabel_8);
 		
 		JLabel lblNewLabel_9 = new JLabel("Square Feet");
-		lblNewLabel_9.setBounds(102, 165, 108, 20);
+		lblNewLabel_9.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		lblNewLabel_9.setBounds(45, 207, 108, 20);
 		frame.getContentPane().add(lblNewLabel_9);
 		
 		JLabel lblNewLabel_10 = new JLabel("Number Of Bedrooms");
-		lblNewLabel_10.setBounds(102, 190, 139, 14);
+		lblNewLabel_10.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		lblNewLabel_10.setBounds(47, 244, 222, 14);
 		frame.getContentPane().add(lblNewLabel_10);
 		
 		JButton btnNewButton = new JButton("New button");
-		btnNewButton.setBounds(10, 215, 200, 50);
+		btnNewButton.setBounds(10, 306, 200, 50);
 		frame.getContentPane().add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("New button");
-		btnNewButton_1.setBounds(220, 215, 200, 50);
+		btnNewButton_1.setBounds(220, 306, 200, 50);
 		frame.getContentPane().add(btnNewButton_1);
 		
 		JButton btnNewButton_2 = new JButton("New button");
-		btnNewButton_2.setBounds(10, 278, 200, 50);
+		btnNewButton_2.setBounds(10, 391, 200, 50);
 		frame.getContentPane().add(btnNewButton_2);
 		
 		JButton btnNewButton_3 = new JButton("New button");
-		btnNewButton_3.setBounds(220, 278, 200, 50);
+		btnNewButton_3.setBounds(220, 391, 200, 50);
 		frame.getContentPane().add(btnNewButton_3);
 		
 		JButton btnNewButton_4 = new JButton("New button");
-		btnNewButton_4.setBounds(10, 341, 200, 50);
+		btnNewButton_4.setBounds(10, 478, 200, 50);
 		frame.getContentPane().add(btnNewButton_4);
 		
 		JButton btnNewButton_5 = new JButton("New button");
-		btnNewButton_5.setBounds(220, 341, 200, 50);
+		btnNewButton_5.setBounds(220, 478, 200, 50);
 		frame.getContentPane().add(btnNewButton_5);
+		
+		textField = new JTextField();
+		textField.setBounds(255, 82, 86, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(255, 118, 86, 20);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(255, 148, 86, 20);
+		frame.getContentPane().add(textField_2);
+		textField_2.setColumns(10);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(255, 176, 86, 20);
+		frame.getContentPane().add(textField_3);
+		textField_3.setColumns(10);
+		
+		textField_4 = new JTextField();
+		textField_4.setBounds(255, 210, 86, 20);
+		frame.getContentPane().add(textField_4);
+		textField_4.setColumns(10);
+		
+		textField_5 = new JTextField();
+		textField_5.setBounds(255, 238, 86, 20);
+		frame.getContentPane().add(textField_5);
+		textField_5.setColumns(10);
 	}
 }
