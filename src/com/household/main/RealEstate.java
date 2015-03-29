40b281bf9cbf018984ca5aa56587f809169b9ee8
PrasadMaduranga03 <prasadmaduranga03@gmail.com>
2015-03-29 22:28:29 +0530

    init

--- src/com/household/main/RealEstate.java
+++ src/com/household/main/RealEstate.java
@@ -3,7 +3,7 @@ package com.household.main;
 import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JButton;
-import javax.swing.JOptionPane;
+//import javax.swing.JOptionPane;
 import javax.swing.JLabel;
 
 
