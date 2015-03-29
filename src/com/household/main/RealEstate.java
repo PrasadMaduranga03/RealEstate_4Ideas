7b3f7b8734d4f9ff8ae680af6a1f60e2b339f2db
taniyadilshani <taniyapalihakkara@gmail.com>
2015-03-29 16:49:37 +0530

    define variable names for the text boxes and lables in  the interface

--- src/com/household/main/RealEstate.java
+++ src/com/household/main/RealEstate.java
@@ -16,13 +16,13 @@ public class RealEstate {
 	
 	
 
-	private JFrame frame;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
+	private JFrame frmRealEstate;
+	private JTextField textLotNumber;
+	private JTextField textFName;
+	private JTextField textLName;
+	private JTextField textPrice;
+	private JTextField textSquareFeet;
+	private JTextField textNoOfBedroom;
 
 	/**
 	 * Launch the application.
@@ -32,7 +32,7 @@ public class RealEstate {
 			public void run() {
 			 	try {
 					RealEstate window = new RealEstate();
-					window.frame.setVisible(true);
+					window.frmRealEstate.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -51,99 +51,114 @@ public class RealEstate {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 490, 593);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		frmRealEstate = new JFrame();
+		frmRealEstate.setBounds(100, 100, 490, 593);
+		frmRealEstate.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmRealEstate.getContentPane().setLayout(null);
 		
-		JLabel lblNewLabel_4 = new JLabel("Next house displayed");
-		lblNewLabel_4.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		lblNewLabel_4.setForeground(new Color(0, 0, 205));
-		lblNewLabel_4.setBounds(10, 11, 287, 31);
-		frame.getContentPane().add(lblNewLabel_4);
+		JLabel lblNexthousedisplay = new JLabel("Next House Display");
+		lblNexthousedisplay.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblNexthousedisplay.setForeground(new Color(0, 0, 205));
+		lblNexthousedisplay.setBounds(10, 11, 287, 31);
+		frmRealEstate.getContentPane().add(lblNexthousedisplay);
 		
-		JLabel lblNewLabel_5 = new JLabel("Lot Number");
-		lblNewLabel_5.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblNewLabel_5.setBounds(45, 82, 129, 14);
-		frame.getContentPane().add(lblNewLabel_5);
+		JLabel lblLotNumber = new JLabel("Lot Number : ");
+		lblLotNumber.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		lblLotNumber.setBounds(45, 82, 129, 14);
+		frmRealEstate.getContentPane().add(lblLotNumber);
 		
-		JLabel lblNewLabel_6 = new JLabel("First Name");
-		lblNewLabel_6.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblNewLabel_6.setBounds(45, 118, 108, 14);
-		frame.getContentPane().add(lblNewLabel_6);
+		JLabel lblNewLabel = new JLabel("First Name :");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		lblNewLabel.setBounds(45, 118, 108, 14);
+		frmRealEstate.getContentPane().add(lblNewLabel);
 		
-		JLabel lblNewLabel_7 = new JLabel("Last Name");
-		lblNewLabel_7.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblNewLabel_7.setBounds(45, 154, 108, 14);
-		frame.getContentPane().add(lblNewLabel_7);
+		JLabel lblNewLabel_1 = new JLabel("Last Name :");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		lblNewLabel_1.setBounds(45, 154, 108, 14);
+		frmRealEstate.getContentPane().add(lblNewLabel_1);
 		
-		JLabel lblNewLabel_8 = new JLabel("Price");
-		lblNewLabel_8.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblNewLabel_8.setBounds(45, 182, 46, 14);
-		frame.getContentPane().add(lblNewLabel_8);
+		JLabel lblP = new JLabel("Price :");
+		lblP.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		lblP.setBounds(45, 188, 165, 14);
+		frmRealEstate.getContentPane().add(lblP);
 		
-		JLabel lblNewLabel_9 = new JLabel("Square Feet");
-		lblNewLabel_9.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblNewLabel_9.setBounds(45, 207, 108, 20);
-		frame.getContentPane().add(lblNewLabel_9);
+		JLabel lblNewLabel_2 = new JLabel("Square Feet :");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		lblNewLabel_2.setBounds(45, 213, 108, 20);
+		frmRealEstate.getContentPane().add(lblNewLabel_2);
 		
-		JLabel lblNewLabel_10 = new JLabel("Number Of Bedrooms");
-		lblNewLabel_10.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblNewLabel_10.setBounds(47, 244, 222, 14);
-		frame.getContentPane().add(lblNewLabel_10);
+		JLabel lblNewLabel_3 = new JLabel("Number Of Bedrooms :");
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		lblNewLabel_3.setBounds(45, 244, 222, 14);
+		frmRealEstate.getContentPane().add(lblNewLabel_3);
 		
-		JButton btnNewButton = new JButton("New button");
-		btnNewButton.setBounds(10, 306, 200, 50);
-		frame.getContentPane().add(btnNewButton);
+		JButton btnReset = new JButton("Reset");
+		btnReset.setFont(new Font("Tahoma", Font.BOLD, 13));
+		btnReset.setBounds(10, 306, 200, 50);
+		frmRealEstate.getContentPane().add(btnReset);
 		
-		JButton btnNewButton_1 = new JButton("New button");
-		btnNewButton_1.setBounds(220, 306, 200, 50);
-		frame.getContentPane().add(btnNewButton_1);
+		JButton btnNext = new JButton("Next");
+		btnNext.setFont(new Font("Tahoma", Font.BOLD, 13));
+		btnNext.setBounds(220, 306, 200, 50);
+		frmRealEstate.getContentPane().add(btnNext);
 		
-		JButton btnNewButton_2 = new JButton("New button");
-		btnNewButton_2.setBounds(10, 391, 200, 50);
-		frame.getContentPane().add(btnNewButton_2);
+		JButton btnAdd = new JButton("Add");
+		btnAdd.setFont(new Font("Tahoma", Font.BOLD, 13));
+		btnAdd.setBounds(10, 391, 200, 50);
+		frmRealEstate.getContentPane().add(btnAdd);
 		
-		JButton btnNewButton_3 = new JButton("New button");
-		btnNewButton_3.setBounds(220, 391, 200, 50);
-		frame.getContentPane().add(btnNewButton_3);
+		JButton btnDelete = new JButton("Delete");
+		btnDelete.setFont(new Font("Tahoma", Font.BOLD, 13));
+		btnDelete.setBounds(220, 391, 200, 50);
+		frmRealEstate.getContentPane().add(btnDelete);
 		
-		JButton btnNewButton_4 = new JButton("New button");
-		btnNewButton_4.setBounds(10, 478, 200, 50);
-		frame.getContentPane().add(btnNewButton_4);
+		JButton btnClear = new JButton("Clear");
+		btnClear.setFont(new Font("Tahoma", Font.BOLD, 13));
+		btnClear.setBounds(10, 478, 200, 50);
+		frmRealEstate.getContentPane().add(btnClear);
 		
-		JButton btnNewButton_5 = new JButton("New button");
-		btnNewButton_5.setBounds(220, 478, 200, 50);
-		frame.getContentPane().add(btnNewButton_5);
+		JButton btnFind = new JButton("Find");
+		btnFind.setFont(new Font("Tahoma", Font.BOLD, 13));
+		btnFind.setBounds(220, 478, 200, 50);
+		frmRealEstate.getContentPane().add(btnFind);
 		
-		textField = new JTextField();
-		textField.setBounds(255, 82, 86, 20);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
+		textLotNumber = new JTextField();
+		textLotNumber.setBounds(255, 82, 165, 20);
+		frmRealEstate.getContentPane().add(textLotNumber);
+		textLotNumber.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(255, 118, 86, 20);
-		frame.getContentPane().add(textField_1);
-		textField_1.setColumns(10);
+		textFName = new JTextField();
+		textFName.setBounds(255, 118, 165, 20);
+		frmRealEstate.getContentPane().add(textFName);
+		textFName.setColumns(10);
 		
-		textField_2 = new JTextField();
-		textField_2.setBounds(255, 148, 86, 20);
-		frame.getContentPane().add(textField_2);
-		textField_2.setColumns(10);
+		textLName = new JTextField();
+		textLName.setBounds(255, 148, 165, 20);
+		frmRealEstate.getContentPane().add(textLName);
+		textLName.setColumns(10);
 		
-		textField_3 = new JTextField();
-		textField_3.setBounds(255, 176, 86, 20);
-		frame.getContentPane().add(textField_3);
-		textField_3.setColumns(10);
+		textPrice = new JTextField();
+		textPrice.setBounds(255, 176, 165, 20);
+		frmRealEstate.getContentPane().add(textPrice);
+		textPrice.setColumns(10);
 		
-		textField_4 = new JTextField();
-		textField_4.setBounds(255, 210, 86, 20);
-		frame.getContentPane().add(textField_4);
-		textField_4.setColumns(10);
+		textSquareFeet = new JTextField();
+		textSquareFeet.setBounds(255, 210, 165, 20);
+		frmRealEstate.getContentPane().add(textSquareFeet);
+		textSquareFeet.setColumns(10);
 		
-		textField_5 = new JTextField();
-		textField_5.setBounds(255, 238, 86, 20);
-		frame.getContentPane().add(textField_5);
-		textField_5.setColumns(10);
+		textNoOfBedroom = new JTextField();
+		textNoOfBedroom.setBounds(255, 238, 165, 20);
+		frmRealEstate.getContentPane().add(textNoOfBedroom);
+		textNoOfBedroom.setColumns(10);
+	}
+	
+	public void clearTextFields(){
+		textFName.setText("");
+		textLName.setText("");
+		textLotNumber.setText("");
+		textNoOfBedroom.setText("");
+		textPrice.setText("");
+		textSquareFeet.setText("");
 	}
 }
