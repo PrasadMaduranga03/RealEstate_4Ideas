cc2a5eddb84dbb14b97edba7d3376d00d85f26ac
Your Name <you@example.com>
2015-04-06 08:22:51 +0530

    changed one parameter of file writer.

--- src/com/household/persistence/HouseFile.java
+++ src/com/household/persistence/HouseFile.java
@@ -25,7 +25,7 @@ public class HouseFile {
 				file.createNewFile();
 			} 
 				
-				FileWriter writer = new FileWriter(file.getAbsoluteFile(), true);
+				FileWriter writer = new FileWriter(file.getAbsoluteFile(), newfile);
 				BufferedWriter lno = new BufferedWriter(writer);
 				// http://www.mkyong.com/java/how-to-export-data-to-csv-file-java/
 				for (int i = 0; i < 1; i++) {
